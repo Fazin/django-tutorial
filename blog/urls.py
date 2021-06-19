457808cd50fac57b991f42b45108c3b8ea105edb
fabriciopassos <fabricio@infrasolar.com.br>
2021-06-19 14:33:59 -0300

    conectando urls a View

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 
 from . import views
 
-app_name = 'blog'
+app_name = 'blog' ##variavel importante para referenciar as urls desse arquivo
+
+#as_view para tornar callable
+#name para referenciar e evitar conflito entre urls com msm nome
 
 urlpatterns = [
-    
-]
+    path("", views.PostListView.as_view(), name="list"),
+    path("<slug:slug>/", views.PostListView.as_view(), name="detail"),
+]  ## lista de padroes de url, url sem argumentos vai na lista de post, com slug vai para pagina de um post
